043d529b9dffab3757c70856747226a7558b5780
folknik <fadeev1087@gmail.com>
2020-10-23 15:28:25 +0300

    added new stocks

--- moex.py
+++ moex.py
@@ -1,6 +1,7 @@
 import sys
 import requests
 from time import sleep
+from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
@@ -33,22 +34,26 @@ def bot_sendtext(message):
 def get_price(driver, link):
     driver.get(link)
     price = driver.find_element_by_xpath("//*[@id='last_last']").text
-    return price
+    # diff = driver.find_element_by_xpath("//*[@id='quotes_summary_current_data']/div[1]/div[2]/div[1]/span[2]").text
+    percent = driver.find_element_by_xpath("//*[@id='quotes_summary_current_data']/div[1]/div[2]/div[1]/span[4]").text
+    return price, percent
 
 
 if __name__ == '__main__':
     stocks = [
-        ['GAZP', 'https://ru.investing.com/equities/gazprom_rts', 'RUB'],
-        ['AFLT', 'https://ru.investing.com/equities/aeroflot', 'RUB']
+        ['GAZP', 'https://ru.investing.com/equities/gazprom_rts', 'р'],
+        ['TATN', 'https://ru.investing.com/equities/tatneft_rts', 'р'],
+        ['SBER', 'https://ru.investing.com/equities/sberbank_rts', 'р'],
+        ['GMKN', 'https://ru.investing.com/equities/gmk-noril-nickel_rts', 'р'],
+        ['AFLT', 'https://ru.investing.com/equities/aeroflot', 'р'],
     ]
     options = WebOptions().extract
     driver = webdriver.Chrome(executable_path='/Users/afadeev/Documents/chromedriver', options=options)
     while True:
         try:
-            for stock in stocks:
-                price = get_price(driver, stock[1])
-                print(stock[0], price)
-                bot_sendtext(stock[0])
+            stock_prices = [get_price(driver, stock[1]) for stock in stocks]
+            message_list = ["{}: price {} {}, diff {}".format(s[0], p[0], s[2], p[1]) for p, s in zip(stock_prices, stocks)]
+            bot_sendtext("\n".join(message_list))
             sleep(60)
         except KeyboardInterrupt:
             driver.close()
