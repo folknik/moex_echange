51ddc590cf0981a94f9f381dd7ea6f5d7e9571ab
folknik <fadeev1087@gmail.com>
2020-10-23 15:31:44 +0300

    fixed bugs

--- moex.py
+++ moex.py
@@ -33,18 +33,17 @@ def bot_sendtext(message):
 
 def get_price(driver, link):
     driver.get(link)
-    price = driver.find_element_by_xpath("//*[@id='last_last']").text
-    # diff = driver.find_element_by_xpath("//*[@id='quotes_summary_current_data']/div[1]/div[2]/div[1]/span[2]").text
-    percent = driver.find_element_by_xpath("//*[@id='quotes_summary_current_data']/div[1]/div[2]/div[1]/span[4]").text
+    price = driver.find_element_by_xpath("//*[@id='last_last']").text.replace(',', '.')
+    percent = driver.find_element_by_xpath("//*[@id='quotes_summary_current_data']/div[1]/div[2]/div[1]/span[4]").text.replace(',', '.')
     return price, percent
 
 
 if __name__ == '__main__':
     stocks = [
-        ['GAZP', 'https://ru.investing.com/equities/gazprom_rts', 'р'],
-        ['TATN', 'https://ru.investing.com/equities/tatneft_rts', 'р'],
-        ['SBER', 'https://ru.investing.com/equities/sberbank_rts', 'р'],
-        ['GMKN', 'https://ru.investing.com/equities/gmk-noril-nickel_rts', 'р'],
+        ['GAZP', 'https://ru.investing.com/equities/gazprom_rts', 'RUB'],
+        ['TATN', 'https://ru.investing.com/equities/tatneft_rts', 'RUB'],
+        ['SBER', 'https://ru.investing.com/equities/sberbank_rts', 'RUB'],
+        ['GMKN', 'https://ru.investing.com/equities/gmk-noril-nickel_rts', 'RUB'],
         ['AFLT', 'https://ru.investing.com/equities/aeroflot', 'р'],
     ]
     options = WebOptions().extract
@@ -52,7 +51,7 @@ if __name__ == '__main__':
     while True:
         try:
             stock_prices = [get_price(driver, stock[1]) for stock in stocks]
-            message_list = ["{}: price {} {}, diff {}".format(s[0], p[0], s[2], p[1]) for p, s in zip(stock_prices, stocks)]
+            message_list = ["{}: {} {}, {}".format(s[0], p[0], s[2], p[1]) for p, s in zip(stock_prices, stocks)]
             bot_sendtext("\n".join(message_list))
             sleep(60)
         except KeyboardInterrupt:
